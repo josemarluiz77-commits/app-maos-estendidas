fda89712c44584fd986a5cd830af30f3dc427e84
josemarluiz77-commits <josemar.luiz77@gmail.com>
2026-05-28 14:00:07 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,83 +1,21 @@
-const CACHE_NAME = "maos-estendidas-v5";
-
-const urlsToCache = [
-
-"./",
-"./index.html",
-"./admin.html",
-"./login.html",
-"./manifest.json",
-"./icon-192.png",
-"./icon-512.png"
-
-];
-
-/* INSTALAR */
-
 self.addEventListener("install", event=>{
-
-event.waitUntil(
-
-caches.open(CACHE_NAME)
-
-.then(cache=>{
-
-return cache.addAll(urlsToCache);
-
-})
-
-);
 
 self.skipWaiting();
 
 });
 
-
-/* ATIVAR */
-
 self.addEventListener("activate", event=>{
 
 event.waitUntil(
-
-caches.keys()
-
-.then(keys=>{
-
-return Promise.all(
-
-keys.map(key=>{
-
-if(key !== CACHE_NAME){
-
-return caches.delete(key);
-
-}
-
-})
-
-);
-
-})
-
+clients.claim()
 );
 
 });
 
-
-/* BUSCAR */
-
 self.addEventListener("fetch", event=>{
 
 event.respondWith(
-
-caches.match(event.request)
-
-.then(response=>{
-
-return response || fetch(event.request);
-
-})
-
+fetch(event.request)
 );
 
 });
